60a12b9f014c33840c4605b8a9581f3309519469
SohumK <4sohum@gmail.com>
2019-06-26 01:13:20 -0400

    Create init.js

---
+++ JS/init.js
@@ -0,0 +1,30 @@
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.2.3/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBEZtRIahIHeHy_8K6yDW1oMABz7GJJWjg",
+    authDomain: "skwebsite-80d06.firebaseapp.com",
+    databaseURL: "https://skwebsite-80d06.firebaseio.com",
+    projectId: "skwebsite-80d06",
+    storageBucket: "",
+    messagingSenderId: "708858055954",
+    appId: "1:708858055954:web:390198b36f70c240"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141267370-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-141267370-1');
+    </script>
